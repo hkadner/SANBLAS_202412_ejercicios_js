6ce2a8105c423756b35e9982aa7d225bbdb1836f
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-12-12 13:57:21 +0100

    Update ejercicio_27_fetch.js

--- ejercicio_27_fetch.js
+++ ejercicio_27_fetch.js
@@ -1,7 +1,36 @@
 const URL = 'https://fpaniaguaangular.github.io/gamecovers/assets/gamecovers/gamecovers.json';
-function processData(data){
-    console.log(data);
+
+
+function processData(data) {//data (en este caso) es un array
+    /*
+    data.forEach(consola => {
+        console.log(consola.name);
+    });
+    */
+    /*
+     const consolasVerticales =
+         data.filter(consola => {
+             return consola.orientation === 'portrait';
+         });
+ 
+     consolasVerticales.forEach(consola => {
+         console.log(consola.name);
+     });
+     */
+
+    /*
+   const consolasNintendo = data.filter(consola => {
+       return consola.name.toUpperCase().includes("NINTENDO");
+   });
+   consolasNintendo.forEach(consola => {
+       console.log(consola.name);
+   });
+   */
+
+    data.filter(consola => consola.name.toUpperCase().includes("NINTENDO")).forEach(element => console.log(element.name));
+
 }
+
 function doRequest(url) {
     fetch(url)
         .then((response) => {
@@ -13,7 +42,7 @@ function doRequest(url) {
         .then((data) => {
             processData(data);
         })
-        .catch((error) => console.error("Fetch error:", error)); // In case of an error, it
+        .catch((error) => console.error("Fetch error:", error));
 }
 
 doRequest(URL);
