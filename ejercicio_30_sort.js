d8b4d49594bbb151601f77ebb0b8412f2101aba9
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-12-16 10:45:01 +0100

    Create ejercicio_30_sort.js

---
+++ ejercicio_30_sort.js
@@ -0,0 +1,35 @@
+const dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+const numeros = [8, 10, -3, 2, 200, 4];
+
+numeros.sort();
+console.log(numeros);//[-3, 10, 2, 200, 4, 8]
+
+dias.sort();
+console.log(dias);//['Domingo', 'Jueves', 'Lunes', 'Martes', 'Miércoles', 'Sábado', 'Viernes'];
+
+numeros.sort((a,b)=> {
+    if (a<b) return -1;
+    if (a>b) return 1;
+    return 0;
+})
+console.log(numeros);
+
+const coche1 = {
+    nombre : 'Seat 600',
+    capacidad : 4,
+    velocidad : 98
+}
+
+const coche2 = {
+    nombre : 'Ferrari F40',
+    capacidad : 2,
+    velocidad : 280
+}
+
+const coche3 = {
+    nombre : 'Skoda Fabia',
+    capacidad : 5,
+    velocidad : 145
+}
+
+let coches = [coche1, coche2, coche3];
