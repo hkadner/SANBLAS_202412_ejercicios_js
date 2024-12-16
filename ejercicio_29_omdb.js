6eb24a256cb845c1de789cfc7baa1f5898a59713
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-12-16 10:08:02 +0100

    Update ejercicio_29_omdb.js

--- ejercicio_29_omdb.js
+++ ejercicio_29_omdb.js
@@ -3,11 +3,23 @@ const COMMON_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
 
 //'https://www.omdbapi.com/?apikey=fe486a03&t=Star Wars'
 
-function processData(pelicula){
-    console.log(pelicula);
+function processDataExercise01(pelicula){
+    console.log(pelicula.Title);
+    console.log(pelicula.Director);
+    let actores = pelicula.Actors.split(',').map(actor=>actor.trim());
+    actores.forEach(actor => {
+        console.log(`Actor:*${actor}*`);
+    });
+}
+function processDataExercise02(searchObject) {
+    const peliculas = searchObject.Search;
+    peliculas.filter(pelicula=>parseInt(pelicula.Year)>=2000).forEach(pelicula=> {
+        console.log(`Título:${pelicula.Title}`);
+        console.log(`Año:${pelicula.Year}`);
+    });
 }
 
-function doRequest(url) {
+function doRequest(url, processData) {
     fetch(url)
         .then((response) => {
             if (!response.ok) {
@@ -21,10 +33,14 @@ function doRequest(url) {
         .catch((error) => console.error("Fetch error:", error));
 }
 //Ejercicio 1
-doRequest(COMMON_URL + 't=Batman');
 //Mostrar por pantalla el título, el director y los actores (por separado, hay que utilizar split).
+//const URL_REQUEST = COMMON_URL + 't=Batman';
+//console.log(URL_REQUEST);
+//doRequest(URL_REQUEST, processDataExercise01);
 
 //Ejercicio 2
-//doRequest(COMMON_URL + 's=Star Wars');
 //Mostrar el título y el año de las películas estrenadas en el año 2000 y sucesivos
+const URL_REQUEST = COMMON_URL + 's=Star Wars';
+//console.log(URL_REQUEST);
+doRequest(URL_REQUEST, processDataExercise02);
 
