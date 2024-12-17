495e41e6d6b74b8d15c53fdb3841d9d735dd3741
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-12-17 11:10:53 +0100

    Ejercicio 32 - incompleto

--- ejercicio_32/app.js
+++ ejercicio_32/app.js
@@ -1,7 +1,61 @@
 const URL='https://fpaniaguajavascript.github.io/movies-250.json';
 
+function generateCard(pelicula){
+    //1. Crear la tarjeta
+    const nuevaCard = document.createElement("div");//Crea un elemento de tipo div
+    nuevaCard.setAttribute("class","card");
+    //2. Crear la imagen
+    const nuevaImg = document.createElement("img");
+    nuevaImg.setAttribute("src", pelicula.Poster);
+    nuevaImg.setAttribute("alt", `Póster de la película ${pelicula.Title}`);
+    nuevaCard.appendChild(nuevaImg);
+    //3. Crear el contenido de la tarjeta
+    const nuevoContenido = document.createElement("div");
+    nuevoContenido.setAttribute("class","card-content");
+    nuevaCard.appendChild(nuevoContenido);
+    //4. Crear el h3 del título <h3>El Padrino</h3>
+    const nuevoTitulo = document.createElement("h3");
+    nuevoTitulo.textContent = pelicula.Title;
+    nuevoContenido.appendChild(nuevoTitulo);
+    //5. Crear el director <p><strong>Director:</strong> Francis Ford Coppola</p>
+    const nuevoParrafoDirector = document.createElement("p");
+    const nuevaNegrita = document.createElement("strong");
+    nuevoParrafoDirector.appendChild(nuevaNegrita);
+    nuevaNegrita.textContent = "Director: ";
+    nuevoContenido.appendChild(nuevoParrafoDirector);
+    const textoDirector = document.createTextNode(pelicula.Director)
+    nuevoParrafoDirector.appendChild(textoDirector);
+
+
+    
+    //Último paso: Agregar al contenedor la ficha recién creada
+    document.querySelector("#container").appendChild(nuevaCard);//Agregamos el div al contenedor
+}
+
 function processMovie(data) {
-    console.log(data);
+    const peliculas = data.movies;
+    //FORMAS DE RECORRER ARRAYS Y OBJETOS
+    /*
+    //Recorremos con bucle for tradicional
+    for (let i=0;i<peliculas.length;i++){
+        console.log("Duration:" + peliculas[i].Runtime);
+    }
+    //Recorremos con bucle for-of
+    for (pelicula of peliculas) {
+        console.log("Director:" + pelicula.Director);
+    }
+    //Recorremos con el método forEach
+    peliculas.forEach(pelicula => {
+        console.log("Título:" + pelicula.Title);
+    });
+    //Recorre el contenido de un objeto
+    for (atributo in peliculas[0]){
+        console.log(atributo, peliculas[0][atributo]);
+    }
+    */
+    peliculas.forEach(pelicula => {
+        generateCard(pelicula);
+    });
 }
 
 doGetRequest(URL, processMovie);
